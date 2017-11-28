4f8b53943229f99535b0788e3ec8597fe400c708
Rita Maestri <rita.maestri95@gmail.com>
2017-11-28 20:18:30 +0100

    qtgrafica bit

---
+++ qtgrafica/animaparete.cpp
@@ -0,0 +1,61 @@
+#include "animaparete.h"
+//TODO: load in into qml and display it; write the functions
+
+
+AnimaParete::AnimaParete(QQuickPaintedItem *parent) : QQuickPaintedItem(parent), m_mem_index(0) {
+    setRenderTarget(QQuickPaintedItem::InvertedYFramebufferObject);}
+void AnimaParete::setPaths(QVector<QLine> v) {
+    if(v != m_paths){
+        m_paths = v;
+        emit pathsChanged();
+    }
+}
+void AnimaParete::setMem(QVector<QPoint> mem) {
+    m_mem = mem;
+    setMem_index(0);
+    setEnd(m_mem.length()-1);
+    emit memChanged();
+}
+void AnimaParete::setEnd(int e){
+    m_end = e;
+    emit endChanged();
+}
+int AnimaParete::end() const{
+    return m_end;
+}
+void AnimaParete::setMem_index(int ind) {
+    if(ind != m_mem_index){
+        m_mem_index = ind;
+        emit mem_indexChanged();
+    }
+}
+QVector<QPoint> AnimaParete::mem() const {return m_mem;}
+int AnimaParete::mem_index() const {return m_mem_index;}
+
+QVector<QLine> AnimaParete::paths() const {return m_paths;}
+void AnimaParete::paint(QPainter* painter){
+    painter->setWindow(0,0,m_end_point.x(),m_end_point.y());
+    if(!m_paths.empty()){
+        painter->setRenderHint(QPainter::Antialiasing);
+
+        for(const auto& i:m_paths){
+            if(std::find(m_paths.begin(), m_paths.end(), QLine(i.p2(),i.p1())) != m_paths.end()){
+                painter->setPen(Qt::black);
+            }
+            else if(i.dy() > 0) painter->setPen(Qt::red);
+            else painter->setPen(Qt::blue);
+            QPen fine = painter->pen();
+            fine.setWidthF(0.02);
+            painter->setPen(fine);
+            painter->drawLine(i);
+        }
+    }
+    if(!m_mem.empty()){
+        QPen fine = painter->pen();
+        fine.setWidthF(0.05);
+        fine.setColor(Qt::darkGreen);
+        painter->setPen(fine);
+        painter->drawEllipse(QPointF(m_mem[m_mem_index]),0.1,0.1);
+    }
+}
+
---
+++ qtgrafica/animaparete.h
@@ -0,0 +1,41 @@
+#ifndef ANIMAPARETE_H
+#define ANIMAPARETE_H
+
+#include <QtQuick>
+#include "parete.h"
+
+class AnimaParete : public QQuickPaintedItem
+{
+    Q_OBJECT
+    Q_PROPERTY(QVector<QPoint> mem READ mem WRITE setMem NOTIFY memChanged)
+    Q_PROPERTY(QVector<QLine> paths READ paths WRITE setPaths NOTIFY pathsChanged)
+    Q_PROPERTY(int mem_index READ mem_index WRITE setMem_index NOTIFY mem_indexChanged)
+    Q_PROPERTY(int end READ end WRITE setEnd NOTIFY endChanged)
+    Q_PROPERTY(QPoint end_point MEMBER m_end_point)
+public:
+    AnimaParete(QQuickPaintedItem *parent = 0);
+    void setMem(QVector<QPoint> mem);
+    void setPaths(QVector<QLine> v);
+    void setMem_index(int index);
+    void setEnd(int end);
+    QVector<QPoint> mem() const;
+    QVector<QLine> paths() const;
+    int end() const;
+    int mem_index() const;
+    void paint(QPainter *painter);
+signals:
+    void memChanged();
+    void pathsChanged();
+    void mem_indexChanged();
+    void endChanged();
+private:
+    QVector<QLine> m_paths;
+    QVector<QPoint> m_mem;
+    int m_mem_index;
+    int m_end;
+    QPoint m_end_point;
+};
+Q_DECLARE_METATYPE(QVector<QLine>);
+Q_DECLARE_METATYPE(QVector<QPoint>);
+
+#endif // ANIMAPARETE_H
